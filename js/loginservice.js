213b978ae5075139cc937a87803480a72974ae93
yesica-coronel <ypcoronelm@ufpso.edu.co>
2025-04-09 20:37:54 -0500

    segunda parte

--- js/loginservice.js
+++ js/loginservice.js
@@ -4,6 +4,7 @@ document.getElementById("formLogin").addEventListener('submit', function (e){
     const password = document.getElementById("password").value;
     let message = ''
     let alerType = ''
+    login (email,password)
 
 
     if(email === "" || password === ""){
@@ -29,3 +30,33 @@ document.getElementById("formLogin").addEventListener('submit', function (e){
     `;
     document.getElementById('alert').innerHTML = alert;
 })
+
+function login(email, password){
+    fetch("htpps://reqres.in/api/login",{
+        method: "POST",
+        headers: {
+            "content-type": "aplication/json"
+        },
+        body: JSON.stringify({ email, password}),
+    })
+    .then((data) => {
+         alerType = 'success'
+        message = 'inicio de secion exitoso'
+        console.log('responde bien' + data)
+    })
+       
+    
+    .catch((error) => {
+        alerType = 'danger'
+        message ='Correo o contraseña incorrectos.';
+        console.error(error)
+    })
+
+    let alert = `
+    <div class="alert alert-${alerType} alert-dismissible fade show" role="alert">
+      ${message}
+       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+   </div>
+    `;
+    document.getElementById('alert').innerHTML = alert;
+}
